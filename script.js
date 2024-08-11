73102405b117cca713b1d0eb4ff9fd9de81ce215
MacBookAir <macbookpro@MacBook-Air-MacBook-2.local>
2024-08-11 07:18:16 +0500

    first commit

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ document.addEventListener('DOMContentLoaded', function () {
         navList.classList.toggle('show');
     });
 });
+
+function toggleAnswer(answerId, questionElement) {
+    var answerElement = document.getElementById(answerId);
+    var iconElement = questionElement.querySelector('.icon');
+    if (answerElement.classList.contains('show')) {
+        answerElement.classList.remove('show');
+        iconElement.textContent = '+';
+        iconElement.style.transform = 'rotate(0deg)';
+    } else {
+        answerElement.classList.add('show');
+        iconElement.textContent = '-';
+        iconElement.style.transform = 'rotate(5deg)';
+    }
+}
