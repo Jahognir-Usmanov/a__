61cf65b80ab5656ac49f7b28139f2d6e1da7467b
MacBookAir <macbookpro@MacBook-Air-MacBook-2.local>
2024-08-15 03:32:40 +0500

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-AOS.init();
+// import AOS from 'aos';
+// import 'aos/dist/aos.css';
+// AOS.init();
 
 
 document.addEventListener('DOMContentLoaded', function () {
